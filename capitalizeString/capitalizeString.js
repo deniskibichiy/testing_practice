89a601edb00412a5a68cd1208859f09c58659541
Denis Kibichiy <deniskibichiy@outlook.com>
2026-05-20 07:55:53 +0300

    refactor capitalizeString

--- capitalizeString/capitalizeString.js
+++ capitalizeString/capitalizeString.js
@@ -1,7 +1,6 @@
 function capitalizeString(string) {
-    const firstLetter = 
-    string.slice(0,1).toUpperCase();
-    return `${firstLetter}`+string.slice(1,string.length);
+
+    return string.slice(0,1).toUpperCase()+string.slice(1,string.length);
 };
 
 export {capitalizeString};
